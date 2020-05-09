5347ef627fa946b5e340b2d984bbe5a86246943b
mint611 <las990627@gmail.com>
2020-05-10 04:08:18 +0800

    初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p class="test">nmsl</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  components: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@css/style.scss";
+
+.home {
+  .test {
+    font-size: px2rem(16);
   }
 }
-</script>
+</style>
